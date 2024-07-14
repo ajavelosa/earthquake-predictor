099e84d6890b9d85d54b685865ed200f62968125
Anton J <antonjavelosa@gmail.com>
2024-07-14 10:59:07 +0800

    Using built in model_dumps()

--- earthquake-producer/src/seismic_portal_api/earthquake.py
+++ earthquake-producer/src/seismic_portal_api/earthquake.py
@@ -10,13 +10,3 @@ class Earthquake(BaseModel):
     depth: float
     latitude: float
     longitude: float
-
-    def model_dump(self):
-        return {
-            'timestamp_ms': self.timestamp_ms,
-            'region': self.region,
-            'magnitude': self.magnitude,
-            'depth': self.depth,
-            'latitude': self.latitude,
-            'longitude': self.longitude
-        }
