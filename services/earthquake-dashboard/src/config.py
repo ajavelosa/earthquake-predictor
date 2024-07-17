6d442dbb208e7b4b4007fec43c78da2c68515e6b
Anton J <antonjavelosa@gmail.com>
2024-07-17 12:46:36 +0800

    Dashboard for earthquake data
    - Online mode that reads from the online feature store
    - Offline mode that reads from the offline feature store
    - Frontend that displays a table
    
    TODO:
    - [ ] Display earthquakes on a map instead of a table
    - [ ] Add a data filter for the data
    - [ ] Revisit primary_key granularity; possiby experiment with streaming dataframes
    - [ ] Add a feature to the frontend that allows the user to select the feature store to read from

---
+++ services/earthquake-dashboard/src/config.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+from typing import Optional
+
+
+class Config(BaseSettings):
+    feature_group_name: str
+    feature_group_version: int
+
+    feature_view_name: str
+    feature_view_version: int
+    last_n_days: int = 5
+
+    hopsworks_project_name: str
+    hopsworks_api_key: str
+
+    # live_or_historical: Optional[str] = "live"
+
+    # @field_validator("live_or_historical")
+    # @classmethod
+    # def validate_live_or_historical(cls, value):
+    #     assert value in {
+    #         "live",
+    #         "historical",
+    #     }, f"Invalid value for live_or_historical: {value}"
+    #     return value
+
+config = Config()
