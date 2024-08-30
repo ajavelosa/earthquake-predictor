45e5d07e90238e619653eb5d7090bdd9c94bdba1
Anton J <antonjavelosa@gmail.com>
2024-08-30 09:24:43 +0800

    Removing default values from config

--- services/earthquake_aggregator/src/config.py
+++ services/earthquake_aggregator/src/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
 
-    kafka_broker_address: str = "localhost:19092"
-    kafka_consumer_group: str = "earthquakes_aggregated_consumer_group"
+    kafka_broker_address: str
+    kafka_consumer_group: str
 
-    input_topic: str = "earthquakes_historical"
-    output_topic: str = "earthquakes_aggregated"
+    input_topic: str
+    output_topic: str
 
     window_duration_seconds: int = 60 * 60 # 1 hour
 
